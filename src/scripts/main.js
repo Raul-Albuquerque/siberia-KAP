1c2b0d0b52bb1b8ca4bb8642bc070249658e530f
Raul Albuquerque <raulmalbuquerque2014@gmail.com>
2024-05-29 16:43:37 -0300

    fix: mobile camera bug

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,47 +1,15 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const video = document.querySelector('.video-section__video');
-    const errorMsg = document.createElement('p');
-    errorMsg.className = 'error';
-    document.querySelector('.video-section').appendChild(errorMsg);
+const video = document.querySelector('video')
+const myBtn = document.getElementById('btn')
 
-    function startCamera() {
-        const constraints = {
-            video: {
-                facingMode: "user" // Configuração para usar a câmera frontal
-            }
-        };
+myBtn.addEventListener('click', () => {
 
-        navigator.mediaDevices.getUserMedia(constraints)
-            .then(stream => {
-                video.srcObject = stream;
-                video.play();
-            })
-            .catch(e => {
-                console.error(e);
-                errorMsg.textContent = `Erro ao acessar a câmera: ${e.message}`;
-            });
+    if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia ) {
+        alert('Your browser do not suport this feature')
     }
 
-    if (location.protocol === 'https:' || location.hostname === 'localhost') {
-        startCamera();
-    } else {
-        errorMsg.textContent = 'Acesso à câmera requer HTTPS';
-    }
-
-    // Adicionando evento de visibilidade para pausar a câmera quando a aba não estiver ativa
-    document.addEventListener('visibilitychange', function () {
-        if (document.hidden) {
-        video.pause();
-        } else {
-            video.play();
-        }
-    });
-
-    // Verifique se a câmera está sendo pausada quando a janela é redimensionada (útil para dispositivos móveis)
-    window.addEventListener('resize', function () {
-        if (video.srcObject) {
-            video.play();
-        }
-    });
-
-    });
+    navigator.mediaDevices.getUserMedia({video: true})
+    .then(function (stream) {
+        video.srcObject = stream
+        video.play()
+    })
+})
