7a85d0f09fdc46c8a912d7304f5d559008cae583
Raul Albuquerque <raulmalbuquerque2014@gmail.com>
2024-05-29 15:28:19 -0300

    fix: mobile camera bug

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,21 @@
-let video = document.querySelector('video')
+const video = document.querySelector('video');
+        const errorMsg = document.getElementById('errorMsg');
 
-navigator.mediaDevices.getUserMedia({video:true})
-.then(stream => {
-  video.srcObject = stream
-  video.play()
-})
-.catch(e => console.log(e))
+        function startCamera() {
+            navigator.mediaDevices.getUserMedia({ video: true })
+                .then(stream => {
+                    video.srcObject = stream;
+                    video.play();
+                })
+                .catch(e => {
+                    console.log(e);
+                    errorMsg.textContent = `Erro ao acessar a câmera: ${e.message}`;
+                });
+        }
+
+        // Verifique se a página está sendo servida sobre HTTPS
+        if (location.protocol === 'https:' || location.hostname === 'localhost') {
+            startCamera();
+        } else {
+            errorMsg.textContent = 'Acesso à câmera requer HTTPS';
+        }
