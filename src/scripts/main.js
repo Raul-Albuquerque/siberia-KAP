9a80565c05b3754b73bc2f07c6f0e30c64287215
Raul Albuquerque <raulmalbuquerque2014@gmail.com>
2024-05-29 15:46:14 -0300

    fix: mobile camera fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,21 +1,30 @@
-const video = document.querySelector('video');
-        const errorMsg = document.getElementById('errorMsg');
+document.addEventListener('DOMContentLoaded', function () {
+  const video = document.querySelector('.video-section__video');
+  const errorMsg = document.createElement('p');
+  errorMsg.className = 'error';
+  document.querySelector('.video-section').appendChild(errorMsg);
 
-        function startCamera() {
-            navigator.mediaDevices.getUserMedia({ video: true })
-                .then(stream => {
-                    video.srcObject = stream;
-                    video.play();
-                })
-                .catch(e => {
-                    console.log(e);
-                    errorMsg.textContent = `Erro ao acessar a câmera: ${e.message}`;
-                });
-        }
+  function startCamera() {
+      const constraints = {
+          video: {
+              facingMode: "user" // Configuração para usar a câmera frontal
+          }
+      };
 
-        // Verifique se a página está sendo servida sobre HTTPS
-        if (location.protocol === 'https:' || location.hostname === 'localhost') {
-            startCamera();
-        } else {
-            errorMsg.textContent = 'Acesso à câmera requer HTTPS';
-        }
+      navigator.mediaDevices.getUserMedia(constraints)
+          .then(stream => {
+              video.srcObject = stream;
+              video.play();
+          })
+          .catch(e => {
+              console.log(e);
+              errorMsg.textContent = `Erro ao acessar a câmera: ${e.message}`;
+          });
+  }
+
+  if (location.protocol === 'https:' || location.hostname === 'localhost') {
+      startCamera();
+  } else {
+      errorMsg.textContent = 'Acesso à câmera requer HTTPS';
+  }
+});
